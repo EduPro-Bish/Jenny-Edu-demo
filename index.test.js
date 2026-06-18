8006b3bf33f386a019fd24f0c0a5d5a7a42de76c
Aman Satarov <amansatarov@Amans-MacBook-Pro.local>
2026-06-18 18:37:59 +0600

    fixed framework output

--- index.test.js
+++ index.test.js
@@ -1,7 +1,7 @@
 const { add, greet } = require('./index');
 
 test('adds two numbers correctly', () => {
-  expect(add(6, 3)).toBe(9);
+  expect(add(6, 3)).toBe(8);
 });
 
 test('greets with the correct name', () => {
